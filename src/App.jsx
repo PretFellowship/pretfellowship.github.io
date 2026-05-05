d5a102fe5d40887945e97f623d452e5344f56851
Ben Harrell <benjaminharrell@gmail.com>
2026-05-05 19:36:34 -0400

    redirect to issue for submit

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,10 @@ import { useQuery } from '@tanstack/react-query'
 import EventListView from './views/EventListView'
 import CalendarView from './views/CalendarView'
 import MapView from './views/MapView'
-import SuggestEventForm from './components/SuggestEventForm'
 import './App.css'
 
 function App() {
   const [activeView, setActiveView] = useState('list')
-  const [isSuggestOpen, setIsSuggestOpen] = useState(false)
   const [filters, setFilters] = useState({
     locationType: 'all',
     tags: [],
@@ -102,7 +100,10 @@ function App() {
         >
           🗺️ Map
         </button>
-        <button className="suggest-btn" onClick={() => setIsSuggestOpen(true)}>
+        <button 
+          className="suggest-btn" 
+          onClick={() => window.open('https://github.com/PretFellowship/pretfellowship.github.io/issues', '_blank')}
+        >
           ✉️ Suggest Event
         </button>
       </nav>
@@ -138,8 +139,6 @@ function App() {
           </a>
         </p>
       </footer>
-
-      {isSuggestOpen && <SuggestEventForm onClose={() => setIsSuggestOpen(false)} />}
     </div>
   )
 }
